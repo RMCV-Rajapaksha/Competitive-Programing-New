3962cd2b9ef8cc97afc79aa3b61eb35148048cf9
RMCV-Rajapaksha <147098368+RMCV-Rajapaksha@users.noreply.github.com>
2024-08-08 15:07:26 +0530

    feat: Implement findClosestNumber function for finding the closest number to zero in a list

---
+++ Leetcode/2239. Find Closest Number to Zero.py
@@ -0,0 +1,9 @@
+from typing import List
+
+class Solution:
+    def findClosestNumber(self, nums: List[int]) -> int:
+        closest = nums[0]
+        for x in nums:
+            if abs(x) < abs(closest) or (abs(x) == abs(closest) and x > closest):
+                closest = x
+        return closest
