7a3fcbae5fd4607822c79a08cd4c53cd1129faae
RMCV-Rajapaksha <147098368+RMCV-Rajapaksha@users.noreply.github.com>
2024-08-23 16:40:44 +0530

    Add initial implementation of Valid Parentheses problem

---
+++ Leetcode/20. Valid Parentheses.py
@@ -0,0 +1,19 @@
+class Solution:
+    def isValid(self, s: str) -> bool:
+ 
+        matching_brackets = {')': '(', '}': '{', ']': '['}
+
+        stack = []
+        
+ 
+        for char in s:
+            if char in matching_brackets:
+           
+                top_element = stack.pop() if stack else '#'
+                if matching_brackets[char] != top_element:
+                    return False
+            else:
+
+                stack.append(char)
+
+        return not stack
